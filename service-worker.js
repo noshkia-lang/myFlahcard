58bb3523f73288f7480943819e3b1b6871b00629
noshkia-lang <noshkia@gmail.com>
2025-11-27 13:04:43 +0330

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,2 +1,18 @@
-self.addEventListener("install", e => self.skipWaiting());
-self.addEventListener("fetch", e => {});
+const CACHE_NAME = 'flash-pwa-v1';
+const FILES = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', evt => {
+  evt.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', evt => { evt.waitUntil(clients.claim()); });
+
+self.addEventListener('fetch', evt => {
+  if (evt.request.method !== 'GET') return;
+  evt.respondWith(caches.match(evt.request).then(resp => resp || fetch(evt.request)));
+});
